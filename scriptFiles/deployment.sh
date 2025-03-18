231f3c3f02db4627abb706e193a92c79cc15e958
Rahul Saha <rahulsaha.wbnxb@gmail.com>
2025-03-18 11:51:28 +0530

    deployment.sh file has been updated

--- scriptFiles/deployment.sh
+++ scriptFiles/deployment.sh
@@ -1,1 +1,30 @@
+#!/bin/bash
+
+#define variables
+
+REPO_URL=""
+TARGET_DIR=""
+BRANCH=""
+BUILD_DIR=""
+
+
+#update code from repository
+echo "updating code from branch"
+cd "$TARGET_DIR" || exit
+git pull origin $BRANCH
+
+#build the project 
+echo "Building the project"
+cd "$BUILD_DIR" || exit 
+
+#Deploy the project 
+echo "Deploying the project..."
+cp /*jar /opt/tomcat/webapps/
+
+#restart the service 
+source restart.sh
+
+
+echo "Deployment has been completed"
+
 
